581636b8a14b0d39a100d13e956e7cae42d9f3b5
RobertAKARobin <robertgfthomas@gmail.com>
2016-02-03 16:21:03 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+var game = {
+  deck: [],
+  players: [],
+  hands: [],
+  buildDeck: function(){
+
+  },
+  shuffleDeck: function(){
+
+  },
+  getMorePlayers: function(){
+
+  },
+  deal: function(){
+
+  },
+  findHighestCard: function(){
+
+  },
+  announceWinners: function(){
+
+  },
+  playANewGame: function(){
+
+  }
+}
