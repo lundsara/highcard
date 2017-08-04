87f0512dbf414579bee4266e8db1cae7309824d8
sarah lund <sarah.christine.lund@gmail.com>
2017-08-04 15:20:55 -0400

    adding game logic

--- script.js
+++ script.js
@@ -9,15 +9,10 @@ var game = {
   hands: [],
   counter: 0,
 
-  //HOW DO WE ADD WORTH TO THE CARDS. CAN WE USE THIS:
-  //var worth = values.indexOf(cardValue);
+
   buildDeck(){
-// Populates the deck array with 52 standard playing cards.
-// loop through all values for each suit and attach value to the suit, then loop through all cards and push into the empty deck
+// loop through all values for every suit and attach value to the suit, then loop through all cards and push into the empty deck
 // as you are looping through the array, push into the empty deck
-    //create an empty object card
-    //let card = {};
-    //loop through the suits
     for (let i = 0; i < suits.length; i++) {
     //loop through the values
     for (let j = 0; j < values.length; j++) {
@@ -37,10 +32,7 @@ var game = {
   },
 
   shuffleDeck(){
-  // Randomizes the order of deck.
-  // Math.random(deck)
-  //use Durstenfeld shuffle
-  //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  //used Durstenfeld shuffle https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   //loop through this.deck, each time decreasing by one
     for (var i = this.deck.length - 1; i > 0; i--) {
         //assign j to a random number with the index i + 1
@@ -57,30 +49,23 @@ var game = {
   },
 
   getMorePlayers(){
-//Asks the user if they want to add an additional player to the game.
-//If they do, the player is added to the players array. If not, the script continues.
-//prompt user for an additional player to the game
-//if they answer yes, then push a player into the empty array
-  // get user input and store it in a variable
   let newPlayerInput = prompt('Enter username or press cancel');
   //counter starts at 0
 
     //if they input something
     if (newPlayerInput !== null) {
-      //adding this player at the index of 0 is equal to the object
+      //set play at index zero to empty object
       this.players[this.counter] = {};
-      //adding this player at the index of 0 has been attributed a name which is equal to new player input
+      // attributed a name to object which is equal to new player input
       this.players[this.counter].name = newPlayerInput;
       }
-      //add original player to game
+
       this.counter++;
-      //return the players
+
       return this.players;
   },
 
   deal(){
-//Assigns one card to each player.
-//randomly selects an index from the deck array
     //loop through all of the players
     for (let i = 0; i < this.players.length; i++) {
     //deal out the first card and assign it to variable card
@@ -95,13 +80,13 @@ var game = {
 //Aces are high. For now, don't worry about ties, nor about one suit being more valuable than another suit.
 //compare all of the wins to see who has the highest card (like tic tac toe)
 
-  //if card value of player 1 is higher than the value of player 2, then player 1 wins
+  //if card value of player 1 is higher than the value of player 2 = player 1 wins
   if (this.players[0].card.worth > this.players[1].card.worth){
     alert ('Player 1 wins!');
-  //if card value of player 1 is higher than the value of player 2, then player 1 wins
+  //if card value of player 1 is higher than the value of player 2 = player 1 wins
   } else if (this.players[0].card.worth < this.players[1].card.worth){
     alert ('Player 2 wins!');
-  //if card value of player 1 is equal to the value of player 2, then it's a draw
+  //if card value of player 1 is equal to the value of player 2 = tie game
   } else if (this.players[0].card.worth === this.players[1].card.worth){
     alert ('Draw!');
   }
@@ -123,10 +108,4 @@ var game = {
   }
 }
 
-console.log(game.buildDeck());
-console.log(game.shuffleDeck());
-game.getMorePlayers();
-console.log(game.getMorePlayers());
-game.deal();
-game.findHighestCard();
-//console.log(game.announceWinners());
+
